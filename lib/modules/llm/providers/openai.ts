435184c90e7f6bc70484e9b9318e14b7f818d606
wordix <sparrowwht7@gmail.com>
2025-03-17 15:14:17 +0800

    init

--- lib/modules/llm/providers/openai.ts
+++ lib/modules/llm/providers/openai.ts
@@ -14,53 +14,51 @@ export default class OpenAIProvider extends BaseProvider {
 
   staticModels: ModelInfo[] = [
     // { name: 'gpt-4.5-preview', label: 'GPT-4.5 Preview', provider: 'OpenAI', maxTokenAllowed: 16384 },
-    { name: 'gpt-4o', label: 'GPT-4o', provider: 'OpenAI', maxTokenAllowed: 8000 },
-    { name: 'gpt-4o-mini', label: 'GPT-4o Mini', provider: 'OpenAI', maxTokenAllowed: 8000 },
-    { name: 'gpt-4-turbo', label: 'GPT-4 Turbo', provider: 'OpenAI', maxTokenAllowed: 8000 },
-    { name: 'gpt-4', label: 'GPT-4', provider: 'OpenAI', maxTokenAllowed: 8000 },
+    { name: 'openai/chatgpt-4o-latest', label: 'GPT-4o', provider: 'OpenAI', maxTokenAllowed: 16384 },
+    { name: 'openai/gpt-4o-mini', label: 'GPT-4o Mini', provider: 'OpenAI', maxTokenAllowed: 16384 },
     // { name: 'gpt-3.5-turbo', label: 'GPT-3.5 Turbo', provider: 'OpenAI', maxTokenAllowed: 8000 },
   ];
 
-  async getDynamicModels(
-    apiKeys?: Record<string, string>,
-    settings?: IProviderSetting,
-    serverEnv?: Record<string, string>,
-  ): Promise<ModelInfo[]> {
-    const { apiKey } = this.getProviderBaseUrlAndKey({
-      apiKeys,
-      providerSettings: settings,
-      serverEnv: serverEnv as any,
-      defaultBaseUrlKey: '',
-      defaultApiTokenKey: 'OPENAI_API_KEY',
-    });
+  // async getDynamicModels(
+  //   apiKeys?: Record<string, string>,
+  //   settings?: IProviderSetting,
+  //   serverEnv?: Record<string, string>,
+  // ): Promise<ModelInfo[]> {
+  //   const { apiKey } = this.getProviderBaseUrlAndKey({
+  //     apiKeys,
+  //     providerSettings: settings,
+  //     serverEnv: serverEnv as any,
+  //     defaultBaseUrlKey: '',
+  //     defaultApiTokenKey: 'OPENAI_API_KEY',
+  //   });
 
-    if (!apiKey) {
-      throw `Missing Api Key configuration for ${this.name} provider`;
-    }
+  //   if (!apiKey) {
+  //     throw `Missing Api Key configuration for ${this.name} provider`;
+  //   }
 
-    const response = await fetch(`https://api.openai.com/v1/models`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
+  //   const response = await fetch(`https://api.openai.com/v1/models`, {
+  //     headers: {
+  //       Authorization: `Bearer ${apiKey}`,
+  //     },
+  //   });
 
-    const res = (await response.json()) as any;
-    const staticModelIds = this.staticModels.map((m) => m.name);
+  //   const res = (await response.json()) as any;
+  //   const staticModelIds = this.staticModels.map((m) => m.name);
 
-    const data = res.data.filter(
-      (model: any) =>
-        model.object === 'model' &&
-        (model.id.startsWith('gpt-') || model.id.startsWith('o') || model.id.startsWith('chatgpt-')) &&
-        !staticModelIds.includes(model.id),
-    );
+  //   const data = res.data.filter(
+  //     (model: any) =>
+  //       model.object === 'model' &&
+  //       (model.id.startsWith('gpt-') || model.id.startsWith('o') || model.id.startsWith('chatgpt-')) &&
+  //       !staticModelIds.includes(model.id),
+  //   );
 
-    return data.map((m: any) => ({
-      name: m.id,
-      label: `${m.id}`,
-      provider: this.name,
-      maxTokenAllowed: m.context_window || 32000,
-    }));
-  }
+  //   return data.map((m: any) => ({
+  //     name: m.id,
+  //     label: `${m.id}`,
+  //     provider: this.name,
+  //     maxTokenAllowed: m.context_window || 32000,
+  //   }));
+  // }
 
   getModelInstance(options: {
     model: string;
@@ -82,9 +80,11 @@ export default class OpenAIProvider extends BaseProvider {
       throw new Error(`Missing API key for ${this.name} provider`);
     }
 
+    console.log('apiKey **************', process.env.OPEN_ROUTER_API_KEY, model);
+
     const openai = createOpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-      baseURL: 'https://api.openai-proxy.org/v1',
+      apiKey: process.env.OPEN_ROUTER_API_KEY,
+      baseURL: "https://openrouter.ai/api/v1",
     });
 
     return openai(model);
