61f5072ce3a6c63a43c41317c8f17cf63dd62277
wordix <sparrowwht7@gmail.com>
2025-03-19 17:52:33 +0800

    init

--- app/api/updatefilelist/route.ts
+++ app/api/updatefilelist/route.ts
@@ -4,7 +4,6 @@ import { updateFileList } from '@/utils/machines';
 import { auth } from 'auth';
 
 
-export const runtime = 'edge';
 export async function POST(request: Request) {
   try {
     // Parse the request body
